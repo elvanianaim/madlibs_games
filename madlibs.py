fa0a3caa7265d35931dacfa50fbb7ea2be2d63f3
Elvania <115093556+elvanianaim@users.noreply.github.com>
2022-10-07 21:36:45 +0700

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+color = input("Input color: ")
+noun = input("Input plural nouns: ")
+name = input("Input someone's name: ")
+
+print("Roses are "+ color)
+print( noun +" are blue")
+print("Hey "+ name +", I love you!")
